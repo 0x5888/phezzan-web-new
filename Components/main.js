1e1d46695afe967f84420585e4c26408452eed9c
opeyf <llfs@phezzan.xyz>
2022-04-25 22:14:44 +0800

    replace img

--- Components/main.js
+++ Components/main.js
@@ -94,8 +94,8 @@ export default function Main() {
               <div className={styles.desc}>powered by <div className={styles.imgWrapper}><Image style={{marginRight: 10}} className={styles.austLogo} width={40} height={40} src={AUstImg} alt="AUst logo" /></div> aUST <div className={styles.imgWrapper}><Image className={styles.zkLogo} width={56} height={40} src={ZkImg} alt="zkSync logo" /></div> zkSync 2.0</div>
               <div className={styles.divider}></div>
               <h5> Earn <span>19.45%</span> APY while you trade. 
-                  Testnet live on Rinkeby.
               </h5>
+              <h5 className={styles.testnet}>Testnet live on Rinkeby.</h5>
               <a className={styles.joinNow} href={Testnet} target="_blank" rel="noreferrer">
                 <Image 
                   src={JoinNow}
