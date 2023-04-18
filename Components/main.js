4f5b51335d582f12a41c91f8e64aa98581e86517
0x5888 <99529145+0x5888@users.noreply.github.com>
2023-04-18 23:17:19 +0800

    Update main.js

--- Components/main.js
+++ Components/main.js
@@ -24,7 +24,7 @@ const Telegram = "https://t.me/phezzanprotocol"
 
 const Medium = "https://medium.com/@phezzan "
 
-const Discord = "https://discord.com/invite/sfWVaM4d8e"
+const Discord = "https://discord.gg/etXDy8psHx"
 
 const Testnet = "https://testnet.phezzan.xyz"
 
